e9b8a9e6af60c9237fb76b475764cf85544b3cb4
Explv <qsz@post.com>
2019-01-08 20:32:13 +0000

    Fixing crash when player burns two shrimp in survival section

--- src/script/TutorialIsland.java
+++ src/script/TutorialIsland.java
@@ -4,7 +4,7 @@ import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 import sections.*;
 
-@ScriptManifest(author = "Explv", name = "Explv's Tutorial Island", info = "Completes Tutorial Island", version = 5.8, logo = "")
+@ScriptManifest(author = "Explv", name = "Explv's Tutorial Island", info = "Completes Tutorial Island", version = 5.9, logo = "")
 public final class TutorialIsland extends Script {
 
     private final TutorialSection rsGuideSection = new RuneScapeGuideSection();
