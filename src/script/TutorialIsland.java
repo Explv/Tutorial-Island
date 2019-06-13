9637aae9e3a6c520439d2eda225dbd1844083ec2
Explv <qsz@post.com>
2019-06-13 21:25:32 +0100

    v6.1

--- src/script/TutorialIsland.java
+++ src/script/TutorialIsland.java
@@ -4,7 +4,7 @@ import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 import sections.*;
 
-@ScriptManifest(author = "Explv", name = "Explv's Tutorial Island", info = "Completes Tutorial Island", version = 6.0, logo = "")
+@ScriptManifest(author = "Explv", name = "Explv's Tutorial Island", info = "Completes Tutorial Island", version = 6.1, logo = "")
 public final class TutorialIsland extends Script {
 
     private final TutorialSection rsGuideSection = new RuneScapeGuideSection();
